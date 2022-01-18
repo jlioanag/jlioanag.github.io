6b8837b81b86ffdc3008204c2ae8f3e04cc02709
Julian L <jthemage101@gmail.com>
2022-01-17 19:29:34 -0500

    basic design done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#f9f6f1',
+      'l-purple': '#616794',
+      'midnight': '#253031',
+      'metal': '#51567b',
+      'd-purple': '#404463',
+      'silver': '#ece2d0',
+    },
     extend: {},
   },
   plugins: [],
