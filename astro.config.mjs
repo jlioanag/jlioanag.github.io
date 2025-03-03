ced9921e8427760b93e49dd84ed0e7213aa40f0a
Julian L <jthemage101@gmail.com>
2025-03-02 19:16:35 -0500

    feat: add blog components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
+import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
@@ -12,5 +12,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()]
+  integrations: [mdx(), icon()]
 });
