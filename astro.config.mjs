36d51a8649365bac57742cc6f86e6e17a29354a1
jlioanag <40252721+jlioanag@users.noreply.github.com>
2025-06-24 13:38:36 -0500

    chore: add and use prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jlioanag.github.io',
+  site: "https://jlioanag.github.io",
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()]
+  integrations: [mdx()],
 });
