d429d90d1177e31b26ea719d41c42278f0e647a9
jlioanag <40252721+jlioanag@users.noreply.github.com>
2025-06-25 15:54:10 -0400

    chore: change Link style

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jlioanag.github.io",
@@ -11,5 +13,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
 });
